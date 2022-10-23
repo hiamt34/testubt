a3f7dff4674dd6a24c6a6f149cabcb8f15c01080
tranthanhha123 <d17dccn195@gmai.com>
2022-10-23 17:51:16 +0700

    update run server

--- server.js
+++ server.js
@@ -3,8 +3,22 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+const os = require('os')
 
+const interfaces = os.networkInterfaces();
+let hostIp;
+const getNetworkAddress = () => {
+	for (const name of Object.keys(interfaces)) {
+		for (const interface of interfaces[name]) {
+			const {address, family, internal} = interface;
+			if (family === 'IPv4' && !internal) {
+				return address;
+			}
+		}
+	}
+};
 server.use(middlewares);
 server.use(router);
-
-server.listen(port);
+server.listen(port, '0.0.0.0', () => {
+    console.log('Running at http://localhost:' + String(port) + ", on your network: http://" + String(hostIp) + ":" + String(port));
+  });
